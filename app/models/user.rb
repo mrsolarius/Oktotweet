9de975135c022432cef29e71ead6051be8c2af50
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-04-26 16:27:16 +0200

    Test unitaire

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+
+# Class user
 class User < ApplicationRecord
   # prevent login update
   validates :login, presence: true,
@@ -9,8 +11,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
   validates :location, length: { maximum: 30 }
-
-  # validate :login_not_change
 
   # get user profile image from parameter and set in object
   def profile_img=(user_params)
