d8f3a18adfec5d7aacdd4e5e5945bb8504f9e255
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2021-04-30 09:30:13 +0200

    Mise à jour des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,31 @@
 Rails.application.routes.draw do
   get '/', to: 'users#index', as: 'landing'
-  resources :tweets
-  resources :users
-  get '/following/', to: 'tweets#follow_feed', as: 'follow_feed'
-  get '/hashtags/:hashtag', to: 'tweets#hashtag', as: 'hashtag'
-  get '/auth/:id/login', to: 'users#login', as: 'login'
-  get '/auth/logout', to: 'users#logout', as: 'logout'
+  resources :tweets, except: :edit
+  # user creation include in index view
+  resources :users, except: %i[new]
+  # to see the followers of user
   get '/users/:id/followers', to: 'users#followers', as: 'followers'
+  # to see people who follow user
   get '/users/:id/following', to: 'users#following', as: 'following'
+  # to get user profile image
   get '/asset/users/:id/profile/', to: 'users#file', as: 'user_profile_img'
+
+
+
+  # to connect user with his id
+  get '/auth/:id/login', to: 'users#login', as: 'login'
+  # to disconnect user with his id
+  get '/auth/logout', to: 'users#logout', as: 'logout'
+
+
+  # to get follow feed of authenticated user
+  get '/following/', to: 'tweets#follow_feed', as: 'follow_feed'
+  # to view tweets of an hashtag
+  get '/hashtags/:hashtag', to: 'tweets#hashtag', as: 'hashtag'
+
+  # to follow user
   get '/follow/:follow_user_id', to: 'follows#follow', as: 'follow'
+  # to unfollow user
   get '/unfollow/:follow_user_id', to: 'follows#unfollow', as: 'unfollow'
-  namespace :validators do
-    get :client_uniqueness, to: 'validators#client_uniqueness'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
